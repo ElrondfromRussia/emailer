e53dc7390b5044bdf5f7da62c13ae4dffa77ff35
m.gavrilenko <m.gavrilenko@ngrsoftlab.ru>
2020-11-24 20:37:09 +0300

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ElrondfromRussia/emailer
+
+go 1.13
---
+++ mail_sender_test.go
@@ -0,0 +1,30 @@
+// Copyright 2020 Margaret
+package emailer
+
+import (
+	"fmt"
+	"testing"
+)
+
+/////////////////////////////////////////////////
+// Put correct here before testing.
+func TestSimpleMail(t *testing.T) {
+	s := NewSender(
+		"user@mail.com",
+		"password",
+		"user@mail.com",
+		fmt.Sprintf("%v:%v", "test.com", "587"),
+	)
+
+	err := s.NewMessage("topic", []string{"user@mail.com"}, "test", []AttachData{{
+		fileName: "test.txt",
+		fileData: []byte("test"),
+	}})
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = s.Send()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
